171742f351f0ea97fadb75eaa0f604f5fd567f8c
ana bergese <violetagele22@gmail.com>
2021-07-15 15:06:56 +0200

    Added stimulus and created style for the opacity of the navbar

---
+++ app/javascript/controllers/hello_controller.js
@@ -0,0 +1,21 @@
+// Visit The Stimulus Handbook for more details 
+// https://stimulusjs.org/handbook/introduction
+// 
+// This example controller works with specially annotated HTML like:
+//
+// <div data-controller="hello">
+//   <h1 data-hello-target="output"></h1>
+// </div>
+
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "output" ]
+
+  connect() {
+    console.log('Hello!');
+    // Ex.: to replace the contect of the selected HTML element:
+    // this.outputTarget.textContent = 'Hello, Stimulus!'
+    // Remember to create this file with the name of the data-controller
+  }
+}

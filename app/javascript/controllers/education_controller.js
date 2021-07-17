839b5fc88ebd711121d649e0859677d9e9611d3c
ana bergese <violetagele22@gmail.com>
2021-07-17 16:25:34 +0200

    Added files and starting scss stile of carousel for education section

--- app/javascript/controllers/education_controller.js
+++ app/javascript/controllers/education_controller.js
@@ -9,41 +9,41 @@ import SwiperCore, { Pagination, Navigation, Virtual } from 'swiper/core';
 export default class extends Controller {
 
   connect() {
-    // new Swiper('.swiper-container', {
-    //   // Optional parameters
-    //   direction: 'horizontal',
-    //   loop: true,
-    //   slidesPerView: 3,
-    //   centeredSlides: true,
-    //   spaceBetween: 30,
+    new Swiper('.swiper-container', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 3,
+      centeredSlides: true,
+      spaceBetween: 30,
 
 
-    //   // If we need pagination
-    //   pagination: {
-    //     el: '.swiper-pagination',
-    //     type: 'fraction',
-    //   },
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
 
-    //   // Navigation arrows
-    //   navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    //   },
-    //   virtual: {
-    //       slides: (function () {
-    //         const slides = [];
-    //         for (var i = 0; i < 600; i += 1) {
-    //           slides.push('Slide ' + (i + 1));
-    //         }
-    //         return slides;
-    //       })(),
-    //     },
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      virtual: {
+          slides: (function () {
+            const slides = [];
+            for (var i = 0; i < 600; i += 1) {
+              slides.push('Slide ' + (i + 1));
+            }
+            return slides;
+          })(),
+        },
 
-    //   // And if we need scrollbar
-    //   scrollbar: {
-    //     el: '.swiper-scrollbar',
-    //   },
-    // });
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
 
 
 

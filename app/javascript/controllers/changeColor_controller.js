b03848b3b56ca5eb76334d4ffedb562c1223397c
ana bergese <violetagele22@gmail.com>
2021-07-17 11:20:51 +0200

    fixed navbar: when we scroll up to top, the opacity dissapear

--- app/javascript/controllers/changeColor_controller.js
+++ app/javascript/controllers/changeColor_controller.js
@@ -24,7 +24,28 @@ export default class extends Controller {
     };
 }
 
+// cuando el scroll alcance el top de projects section cambiar el backgrpond body color to blue;
+  // const nav = this.colorTarget;
+  // let topOfNav = nav.offsetTop;
+
+  //   function fixNav() {
+  //     if (window.scrollY >= topOfNav) {
+  //       document.body.style.paddingTop = nav.offsetHeight + 'px';
+  //       document.body.classList.add('fixed-nav');
+  //     } else {
+  //       document.body.classList.remove('fixed-nav');
+  //       document.body.style.paddingTop = 0;
+  //     }
+  //   }
+
+  //   window.addEventListener('scroll', fixNav);
 
 // can we haver remove attribute or set attribute depending of the scroolll? ??
 
 // when mouse is in the div, change color
+
+// window.scrollY + window.innerHeight = nos da el margen botton de dónde estamos en la página.
+// colortarget.height / 2 = vamos a estar a la mitad de la altura del elemento para que se aplique la funcion en este momento.
+
+// si yo hago los background de las sections transparents y luego,
+// cambio el background del body con el scroll
--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,19 +4,20 @@ export default class extends Controller {
   static targets = [ "opacity" ]
 
   connect() {
+      let topOfNav = this.opacityTarget.offsetTop;
       var prevScrollpos = window.pageYOffset; // 0 a 3066
+
       window.addEventListener('scroll', () => {
       var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        this.opacityTarget.classList.add('navbar-white');
-      } else {
+      if (prevScrollpos < currentScrollPos) {
         this.opacityTarget.classList.remove('navbar-white');
+      } else if (currentScrollPos == 0 && topOfNav == 0) {
+        this.opacityTarget.classList.remove('navbar-white');
+      } else if (prevScrollpos > currentScrollPos) {
+        this.opacityTarget.classList.add('navbar-white');
       }
+
       prevScrollpos = currentScrollPos;
     });
   }
 }
-// prevScrollpos >= currentScrollPos = show navbar with new class;
-//  5...4...3...2...1....0
-
-

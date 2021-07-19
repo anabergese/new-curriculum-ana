7bae54c5aa9908284709159781b6c034558907d6
ana bergese <violetagele22@gmail.com>
2021-07-19 12:23:39 +0200

    Adding movement to hobbies card, but still it not complete

--- app/javascript/controllers/hobbies_controller.js
+++ app/javascript/controllers/hobbies_controller.js
@@ -4,9 +4,39 @@ export default class extends Controller {
   static targets = [ "move" ]
 
   connect() {
-    console.log('Hello hobbies controller!');
+
+
+
+      const img = this.moveTarget;
+      let currentpos = 0;
+
+      function movething() {
+
+        currentpos += 3;
+        img.style.left = currentpos + "px";
+
+        if ( Math.abs(currentpos >= 900)) {
+          currentpos = -1000;
+        }
+
+        window.requestAnimationFrame(movething);
+      }
+
+      movething();
+
+
+
+
+    }
+
     // Ex.: to replace the contect of the selected HTML element:
     // this.outputTarget.textContent = 'Hello, Stimulus!'
     // Remember to create this file with the name of the data-controller
   }
-}
+
+
+ // o === L ? (e.x >= e.max && (e.x = e.min),
+ //                t.style.transform = "translateX(".concat(e.x, "%)"),
+ //                e.x += a) : (e.x <= e.min && (e.x = e.max),
+ //                t.style.transform = "translateX(".concat(e.x, "%)"),
+ //                e.x -= a)

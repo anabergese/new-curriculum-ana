4db1b504e3a06a3aece63c0ef17c5fcce3a997fb
ana bergese <violetagele22@gmail.com>
2021-08-03 12:54:10 +0200

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'about', to: 'cover#about'
-  get 'about/advertiser_support', to: 'cover#advertiser_support'
-  get 'about/npaw', to: 'cover#npaw'
-  get 'about/content_moderator', to: 'cover#content_moderator'
-  get 'about/customer_service', to: 'cover#customer_service'
-  get 'about/tourist_guide', to: 'cover#tourist_guide'
-  get 'about/marketing', to: 'cover#marketing'
+  root to: 'cover#about'
+  get '/advertiser_support', to: 'cover#advertiser_support'
+  get '/npaw', to: 'cover#npaw'
+  get '/content_moderator', to: 'cover#content_moderator'
+  get '/customer_service', to: 'cover#customer_service'
+  get '/tourist_guide', to: 'cover#tourist_guide'
+  get '/marketing', to: 'cover#marketing'
 end
